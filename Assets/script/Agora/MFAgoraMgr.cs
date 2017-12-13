208cba03bc6907eeb67443623ab2fe31d441c5da
caonimeia <302683523@qq.com>
2017-12-13 20:11:07 +0800

    添加声网SDK插件

---
+++ Assets/script/Agora/MFAgoraMgr.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MFAgoraMgr {
+    private static string APP_ID = "4f9373a3ea474adbb9412cafb32e194f";
+}
